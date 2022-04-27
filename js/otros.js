f6d7d9e0e58784801aa6d4829dc6b3b08f019eca
Valentin Gamboa <104500158+gamboavalentin@users.noreply.github.com>
2022-04-27 20:21:17 -0300

    Update otros.js

--- js/otros.js
+++ js/otros.js
@@ -6,5 +6,35 @@ var otros = {
         billetes de diferente valor, con retención de los mismos y con montura para
         la pared.`,
         impresion3d: {duracion: "35hs", material: {cantidad: "500g", tipo: "PLA"}}
+    },
+    "CLIP CON ROSCA":{
+        title: "CLIP CON ROSCA",
+        img: "clip con rosca.png",
+        text: `Diseño realizado en base a una imagen de internet, consta de tres 
+        partes (tapa, rosca y clip) y permite dosificar cualquier tipo de consumible 
+        en polvo, ya sea yerba mate, cafe, etc. y almacenarlo correctamente.`,
+        impresion3d: {duracion: "5hs", material: {cantidad: "50g", tipo: "PLA"}}
+    },
+    "DESPOLVILLADOR":{
+        title: "DESPOLVILLADOR",
+        img: "despolvillador.png",
+        text: `Diseño realizado en base a un producto comercial, consta de tres partes 
+        (tapa, filtro y base). Sirve para sacar el polvo de la yerba mate.`,
+        impresion3d: {duracion: "8hs", material: {cantidad: "80g", tipo: "PLA"}}
+    },
+    "SOPORTE CELULAR HOMBRESITO":{
+        title: "SOPORTE CELULAR HOMBRESITO",
+        img: "soporte celular hombresito.png",
+        text: `Diseño realizado en base a una imagen de internet, sirve para mantener de
+        manera vertical u orizontal un telefono sobre una superficie plana.`,
+        impresion3d: {duracion: "2hs", material: {cantidad: "30g", tipo: "PLA"}}
+    },
+    "SOPORTE CELULAR TORRE":{
+        title: "SOPORTE CELULAR TORRE",
+        img: "soporte celular torre.png",
+        text: `Diseño realizado en conjunto al cliente, tiene la forma de la torre de
+        control de Rio Grande, sirve como lapicero y ademas como soporte de celular 
+        con dos distancias diferentes.`,
+        impresion3d: {duracion: "14hs", material: {cantidad: "150g", tipo: "PLA"}}
     }
 }
